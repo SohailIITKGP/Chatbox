39bd35e079c346025e4870c92c553456efb8c768
iamsohailbelim <sohailbelim2425@gmail.com>
2024-09-03 00:02:09 +0530

    fix deployment bugs

--- convex/openai.ts
+++ convex/openai.ts
@@ -7,61 +7,55 @@ const apiKey = process.env.OPENAI_API_KEY;
 const openai = new OpenAI({ apiKey });
 
 export const chat = action({
-	args: {
-		messageBody: v.string(),
-		conversation: v.id("conversations"),
-	},
-	handler: async (ctx, args) => {
-		// const res = await openai.chat.completions.create({
-		// 	model: "gpt-3.5-turbo", // "gpt-4" also works, but is so slow!
-		// 	messages: [
-		// 		{
-		// 			role: "system",
-		// 			content: "You are a terse bot in a group chat responding to questions with 1-sentence answers",
-		// 		},
-		// 		{
-		// 			role: "user",
-		// 			content: args.messageBody,
-		// 		},
-		// 	],
-		// });
+args: {
+messageBody: v.string(),
+conversation: v.id("conversations"),
+},
+handler: async (ctx, args) => {
+const res = await openai.chat.completions.create({
+model: "gpt-3.5-turbo", // "gpt-4" also works, but is so slow!
+messages: [
+{
+role: "system",
+content: "You are a terse bot in a group chat responding to questions with 1-sentence answers",
+},
+{
+role: "user",
+content: args.messageBody,
+},
+],
+});
 
-		// const messageContent = res.choices[0].message.content;
+const messageContent = res.choices[0].message.content;
 
-        //when the key is working uncomments the above code and uncomment the below code
-
-		await ctx.runMutation(api.messages.sendChatGPTMessage, {
-			// content: messageContent ?? "I'm sorry, I don't have a response for that",
-            content : "This message is returned as a response from the OpenAI API because the OPENAI key has got expired",
-			conversation: args.conversation,
-			messageType: "text",
-		});
-	},
+await ctx.runMutation(api.messages.sendChatGPTMessage, {
+content: messageContent ?? "I'm sorry, I don't have a response for that",
+conversation: args.conversation,
+messageType: "text",
+});
+},
 });
 
 export const dall_e = action({
-	args: {
-		conversation: v.id("conversations"),
-		messageBody: v.string(),
-	},
-	handler: async (ctx, args) => {
-		// const res = await openai.images.generate({
-		// 	model: "dall-e-3",
-		// 	prompt: args.messageBody,
-		// 	n: 1,
-		// 	size: "1024x1024",
-		// });
+args: {
+conversation: v.id("conversations"),
+messageBody: v.string(),
+},
+handler: async (ctx, args) => {
+const res = await openai.images.generate({
+model: "dall-e-3",
+prompt: args.messageBody,
+n: 1,
+size: "1024x1024",
+});
 
-		// const imageUrl = res.data[0].url;
-
-        //when the key is working uncomments the above code and uncomment the below code
-		await ctx.runMutation(api.messages.sendChatGPTMessage, {
-			// content: imageUrl ?? "/poopenai.png",
-            content : "/poopenai.png",
-			conversation: args.conversation,
-			messageType: "image",
-		});
-	},
+const imageUrl = res.data[0].url;
+await ctx.runMutation(api.messages.sendChatGPTMessage, {
+content: imageUrl ?? "/poopenai.png",
+conversation: args.conversation,
+messageType: "image",
+});
+},
 });
 
 // 1 token ~= 4 chars in English
